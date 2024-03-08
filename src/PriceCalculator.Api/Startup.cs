7641c8f0322676ecab2d21f7e7bf35ea369da9a8
Nikita Kalashnikov <kalashnikov.develop@gmail.com>
2024-03-08 12:41:26 +0500

    add configuration

--- src/PriceCalculator.Api/Startup.cs
+++ src/PriceCalculator.Api/Startup.cs
@@ -1,4 +1,5 @@
-﻿using PriceCalculator.Api.Bll.Services;
+﻿using PriceCalculator.Api.Bll;
+using PriceCalculator.Api.Bll.Services;
 using PriceCalculator.Api.Bll.Services.Interfaces;
 using PriceCalculator.Api.Dal.Repositories;
 using PriceCalculator.Api.Dal.Repositories.Interfaces;
@@ -7,8 +8,18 @@ namespace PriceCalculator.Api;
 
 public class Startup
 {
+    private readonly IConfiguration _configuration;
+
+    public Startup(
+        IConfiguration configuration
+        )
+    {
+        _configuration = configuration;
+    }
+    
     public void ConfigureServices(IServiceCollection services)
     {
+        services.Configure<PriceCalculatorOptions>(_configuration.GetSection("PriceCalculatorOptions"));
         services.AddControllers();
         services.AddEndpointsApiExplorer();
         services.AddSwaggerGen(o =>
