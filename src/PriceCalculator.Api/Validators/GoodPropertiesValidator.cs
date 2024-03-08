ce15485aa63b92b81007aa5040341671cb386b22
Nikita Kalashnikov <kalashnikov.develop@gmail.com>
2024-03-08 13:36:53 +0500

    add fluent validation for DeliveryPrice V2

---
+++ src/PriceCalculator.Api/Validators/GoodPropertiesValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using PriceCalculator.Api.Requests.V2;
+
+namespace PriceCalculator.Api.Validators;
+
+public class GoodPropertiesValidator : AbstractValidator<GoodProperties>
+{
+    public GoodPropertiesValidator()
+    {
+        RuleFor(x => x.Weight)
+            .GreaterThan(0)
+            .LessThan(Int32.MaxValue);
+        
+        RuleFor(x => x.Height)
+            .GreaterThan(0)
+            .LessThan(Int32.MaxValue);
+        
+        RuleFor(x => x.Length)
+            .GreaterThan(0)
+            .LessThan(Int32.MaxValue);
+
+        RuleFor(x => x.Width)
+            .GreaterThan(0)
+            .LessThan(Int32.MaxValue);
+    }
+}
